1ed3cdebcf5689841f30b85f84583c28a5fbc63d
Hannah Lee, Kaila Goff and Maggie Summers <maggie@>
2018-03-01 10:14:56 -0800

    add titlecase callback

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,4 +3,12 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:tags)
   validates(:title, {:presence => true})
   validates(:instructions, {:presence => true})
+  before_save(:titlecase_title)
+
+
+private
+
+  def titlecase_title
+    self.title=(title().titlecase())
+  end
 end
