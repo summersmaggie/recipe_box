69fc8960b467d6fbe801da371808ea8f512c96bc
Hannah Lee, Kaila Goff and Maggie Summers <hannah@>
2018-03-01 08:31:08 -0800

    add first passing validation

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:ingredients)
   has_and_belongs_to_many(:tags)
+  validates(:title, {:presence => true})
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -3,3 +3,10 @@ require('spec_helper')
 describe(Recipe) do
   it { should have_and_belong_to_many(:ingredients) }
 end
+
+describe(Recipe) do
+  it("ensures the name and ingredients are valid") do
+    recipe = Recipe.new({:title => " "})
+    expect(recipe.save()).to(eq(false))
+  end
+end
